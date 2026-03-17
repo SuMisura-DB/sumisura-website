68f0ca5961d3ba9eae21b0eb0976da064ae0f7ee
António Lima <ant.abreulima@gmail.com>
2026-03-17 12:42:59 +0000

    Updated banner video

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -78,7 +78,8 @@
                 <div class="bg-image-container">
                     {{-- <img src="/assets/img/content/hero-bg.jpg"> --}}
                     <video playsinline muted loop autoplay>
-                        <source src="/assets/video/banner-bg.mp4" type="video/mp4">
+                        {{-- <source src="/assets/video/banner-bg.mp4" type="video/mp4"> --}}
+                        <source src="/assets/video/lisboa-banner-bg-cristo-rei.mp4" type="video/mp4">
                     </video>
                 </div>
 
